a1dae17697025310e8953c80897fbb46e9206408
Tony Amirault <tony.amirault@tinyclues.com>
2020-05-02 12:15:33 +0200

    use typicode server

--- src/app/core/article/adapters/article-rest.source.ts
+++ src/app/core/article/adapters/article-rest.source.ts
@@ -6,24 +6,24 @@ import { Article, RawArticle } from "../domain/article.entity";
 
 @Injectable()
 export class ArticleRestSource implements ArticleSource {
+  private readonly baseUrl =
+    "https://my-json-server.typicode.com/amirault/angular5-articles-app";
+
   constructor(private http: HttpClient) {}
 
   public create(newArticle: RawArticle): Observable<Article> {
-    return this.http.post<Article>(
-      "http://localhost:3000/articles/",
-      newArticle
-    );
+    return this.http.post<Article>(`${this.baseUrl}/articles/`, newArticle);
   }
 
   public read(id: number): Observable<Article> {
-    return this.http.get<Article>(`http://localhost:3000/articles/${id}`);
+    return this.http.get<Article>(`${this.baseUrl}/articles/${id}`);
   }
 
   public readAll(): Observable<Article[]> {
-    return this.http.get<Article[]>("http://localhost:3000/articles");
+    return this.http.get<Article[]>(`${this.baseUrl}/articles`);
   }
 
   public delete(id: number): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3000/articles/${id}`);
+    return this.http.delete<any>(`${this.baseUrl}/articles/${id}`);
   }
 }
