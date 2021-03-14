57145b837d6b7a1af2ef32b476fbbb336d40f44a
Tony Amirault <tonyamirault@macbook-pro-tony.home>
2021-03-14 17:20:58 +0100

    update to angular v11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,8 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      { enableTracing: true, relativeLinkResolution: "legacy" } // <-- debugging purposes only
+      // <-- debugging purposes only
     ),
     BrowserModule,
     HttpClientModule,
